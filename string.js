946c73f61f057051c107cc9a8dc78ed0fda18c4a
rohnshrma <rohn11shrma@gmail.com>
2024-03-31 12:01:09 +0530

    indexing and string methods

--- string.js
+++ string.js
@@ -25,9 +25,9 @@
 
 // concatenation : adding two or more strings together
 
-var first = prompt("Enter your first name : ")
-var last = prompt("Enter your last name : ")
-var age = parseInt(prompt("Enter your age :"))
+// var first = prompt("Enter your first name : ")
+// var last = prompt("Enter your last name : ")
+// var age = parseInt(prompt("Enter your age :"))
 
 // var fullName = first + " " + last
 // console.log( "hello world my name is " + first + " " + last+" and my age is " + age);
@@ -42,7 +42,92 @@ var age = parseInt(prompt("Enter your age :"))
 // console.log(12 + -"12");
 
 
-
 // template literals : insertion / injection
 
-console.log(`hello world my name is ${first} ${last} and my age is ${age**2}`)
+// console.log(`hello world my name is ${first} ${last} and my age is ${age**2}`)
+
+
+// indexing  : counting starting from 0, used to access characters of string
+// the first character is always on the 0th position
+// the last character is always on the string.length - 1 position
+// str[index] are used to access the indexing
+
+var txt = "hello world this is rohan sharma."
+
+// console.log(txt[31]);
+
+// console.log(txt[txt.length - 1])
+
+// console.log(txt[0]);
+
+
+// ============ string method
+
+// console.log(txt.at(100))
+// console.log(txt.at(-1))
+// console.log(txt.at(4))
+
+
+
+// console.log(txt.charAt(100));
+// console.log(txt.charAt(-1));
+// console.log(txt.charAt(4));
+
+
+// console.log(txt.concat("\nmy favourite language is hindi.","\nmy age is 27."));
+
+
+var email = "hello@email.com"
+
+// console.log(txt.endsWith("avc"))
+// console.log(txt.endsWith("a.")) 
+// console.log(email.endsWith(".com") && email.includes("@") ) 
+
+
+// console.log(txt.includes("o"));
+// console.log(txt.includes("o",10));
+
+
+
+// console.log(txt.indexOf("o"));
+// console.log(txt.indexOf("o",5));
+// console.log(txt.indexOf("o",8));
+// console.log(txt.indexOf("o",22));
+
+
+
+// console.log(txt.lastIndexOf("o"));
+// console.log(txt.lastIndexOf("o",20));
+
+
+// console.log(txt.startsWith("h"));
+// console.log(txt.startsWith("z"));
+// console.log(txt.startsWith("o",4));
+
+// console.log(txt.repeat(3));
+
+
+// 
+// console.log(txt.slice(0,11));
+// console.log(txt.slice());
+// console.log(txt.slice(undefined,21));
+// console.log(txt.slice(21));
+
+
+
+// console.log(txt[0:10]);
+
+
+
+// console.log(txt.split())
+// console.log(txt.split(""))
+// console.log(txt.split(" "))
+// console.log(txt.split("o"))
+
+
+// mcdonalds => McDonalds
+// MCDONALDS => McDonalds
+
+var brand = prompt("Enter brand :")
+
+console.log(brand.charAt(0).toUpperCase() + brand.charAt(1).toLowerCase() + brand.charAt(2).toUpperCase() + brand.slice(3,).toLowerCase()  );
