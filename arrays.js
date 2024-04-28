e9c1eaaeba47cae4a3d2093f1f98533492c59e02
rohnshrma <rohn11shrma@gmail.com>
2024-04-28 12:30:43 +0530

    hof & cb

--- arrays.js
+++ arrays.js
@@ -53,3 +53,83 @@ var rohan = ["rohan","Sharma","Male",true,26,1.80,["coding","programming"]]
 
 
 
+// higher order functions
+    var numbers = [56, 45, 89, 778, 52, 40, 33, 13];
+var sum = 0;
+
+// for(var i = 0; i < numbers.length; i++){
+//     sumNumber += numbers[i];
+// }
+// console.log(sumNumber);
+
+// numbers.forEach(function(value,index,self){
+// console.log(value,index,self);
+// })
+
+
+// numbers.forEach(function(value,index,self){
+//    sum+=value 
+// })
+
+// console.log(sum);
+
+
+// var cubes= []
+// for(var i = 0; i < numbers.length; i++){
+// var num = numbers[i]
+// cubes.push(num**3)
+
+// }
+// console.log(cubes);
+
+
+
+// map : access each item and perform an action on it : returns a new arrays storing the results
+
+// var cubes = numbers.map(function(value){
+//     return value % 3 ==0 
+// })
+
+// console.log(cubes);
+// var cubes = numbers.map(function(value){
+//     return value ** 3
+// })
+
+// console.log(cubes);
+
+
+
+// var str = ["lovish", "Piyush", "CRLF", "JML", '56'];
+
+// var isVowel = str.map(function(name){
+    //     return name.includes("a") || name.includes("e") || name.includes("i") || name.includes("o") ||name.includes("u") 
+    // })
+    
+    // console.log(isVowel);
+//     var vowelStr = ["CRPF", "Major", "Commando", "ATS"];
+    
+//     var isVowel =[]
+
+// for(var i = 0; i < vowelStr.length; i++){
+//     var newVowels = vowelStr[i];
+//     if(newVowels.includes("a") || newVowels.includes("e") || newVowels.includes("i") || newVowels.includes("o") || newVowels.includes("u")){
+//         isVowel.push(newVowels);
+//     }
+
+// }
+// console.log(isVowel);
+
+
+// var res = numbers.filter (function (num) {
+//     return num % 2 == 0
+// })
+
+// console.log(res);
+
+
+var years = [1995, 2000, 2004, 2021 ,2002];
+
+// var leapYears = years.filter(function(lpYear){
+//     return (lpYear % 4 === 0 && lpYear % 100 !== 0) || (lpYear % 400 === 0) 
+// })
+console.log(leapYears);
--- functions.js
+++ functions.js
@@ -20,3 +20,73 @@
 
 
 // calcBmi(100,1.80)
+
+
+// anonymous function
+
+// 1. assign it as a value to a variable
+// var greet = function (){
+//     console.log('Hello world');
+// }
+
+// greet()
+
+
+// 2. use anonymous function as a callback
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// callback function : when a function is passed into another function as a parameter the passed function is called
+// a callback function 
+
+// var cy = new Date().getFullYear() //global
+
+// function calcAge(){
+//     var cy = new Date().getFullYear() //global
+//     var yob = parseInt(prompt('Enter your year of birth : ')) // local
+
+//     if(yob<1995){
+//         return;
+//     }
+
+//     return cy-yob
+// }
+
+// var age =  calcAge()
+
+// console.log(age)
+
+// console.log(yob);
+
+
+// function lifespan(average_age, callback) {
+//     var age = callback()
+//     var years_left = average_age - age;
+//     console.log(`Year Left : ${years_left}\nMonths Left : ${years_left * 12}\nWeeks Left : ${years_left * 52}\nDays Left : ${years_left * 365}`);
+// }
+
+// lifespan(100, function () {
+//     var cy = new Date().getFullYear() //global
+//     var yob = parseInt(prompt('Enter your year of birth : ')) // local    
+//     if (yob < 1995) {
+//         return;
+//     }
+//     return cy - yob
+// })
